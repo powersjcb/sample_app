8abfbcfc3f4e3f4a062ba6073da2813d29a9aa82
Jacob Powers <powersjcb@gmail.com>
2014-08-10 22:31:50 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,23 +2,40 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
-    it "should have the content 'Sample App" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
+          visit '/static_pages/home'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+
+    it "should not have a custom title page" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).not_to have_title('| Home')
     end
   end
 
 
+  describe "Contact page" do
+    it "Should have title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
 
+    it "should have the content 'Sample Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('contact')
+    end
+  end
 
-
+  
   describe "Help page" do
     it "should have the content 'Help'" do
   	 visit '/static_pages/help'
@@ -26,11 +43,11 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end    
   end
 
-  
+
 
   describe "About page" do
     it "should have the content 'About Us'" do
@@ -39,7 +56,7 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end    
 	end
 end
