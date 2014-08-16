d5765b8635554483c686b95cc6dca60c4061d432
Jacob Powers <powersjcb@gmail.com>
2014-08-16 13:48:32 -0700

    up to 7.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  root 'static_pages#home'
+
+
+
+
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
   # get "static_pages/contact"
   
-  root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
