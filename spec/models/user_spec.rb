d7a354595d707478d47e6915e07daf10cf165ae3
Jacob Powers <powersjcb@gmail.com>
2014-08-22 22:22:59 -0700

    Finished sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,9 @@ describe User do
   it{ should respond_to(:email) }
   it{ should respond_to(:password_digest)}
   it{ should respond_to(:password)}
-  it{ should respond_to(:password_confirmation)}
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it{ should be_valid }
 
@@ -106,6 +108,10 @@ describe "with a password that's too short" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,6 +23,11 @@ describe "AuthenticationPages" do
       it { should have_link('Sign out',       href: signout_path     ) }
       it { should_not have_link( 'Sign in',   href: signin_path      ) }
 
+      describe "followed by signout" do
+        before {click_link "Sign out"}
+        it {should have_link("Sign in")}
+      end
+
     end
 
     describe "invalid sign in" do
@@ -30,6 +35,11 @@ describe "AuthenticationPages" do
 
       it { should have_title( 'Sign in' ) }
       it { should have_selector( 'div.alert.alert-error' ) }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
   end
 end
